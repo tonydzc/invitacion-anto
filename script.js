50114f26d05047b03c8d96897ca23934dee0b13e
Anthony Diaz <anthony.diaz@creativedrive.com>
2020-02-13 15:17:53 -0600

    abrir-cerrar carta animacion

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+var cartaAbierta = false;
 
 $(document.getElementById("carta")).click(function(){
+    
     var carta = document.getElementById("carta");
-    carta.setAttribute("src","images/carta-abierta.png");
+
+    if(cartaAbierta){
+        carta.setAttribute("src","images/carta-cerrada.png");
+        cartaAbierta=false;
+    }else{
+        carta.setAttribute("src","images/carta-abierta.png");
+        cartaAbierta=true;
+    }
+    
+});
+
+
+/* IDEA FOR ICON FROM HERE -> http://dribbble.com/shots/1003867-Dribbble-Invitation?list=popular&offset=2 */
+
+/* Trigger Animation */
+$('a').on('click', function() {
+    $('.container').addClass('is-active');
+    $(this).remove();
 });
