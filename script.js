ce36876aebdfbab135e4f1a6e7ef0e46003f9e0b
Anthony Diaz <anthony.diaz@creativedrive.com>
2020-02-28 15:26:40 -0600

    animacion completada v1

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 var cartaAbierta = false;
+var carta = document.getElementById("carta");
+var cartadiv = $("#carta-div");
 
 $(document.getElementById("carta")).click(function(){
     
-    var carta = document.getElementById("carta");
-    var cartadiv = $("#carta-div");
     if(cartaAbierta){
-        carta.setAttribute("src","images/carta-cerrada.png");
-        cartadiv.removeClass("carta-wrapper-open");
-        cartadiv.addClass("carta-wrapper");
-        cartaAbierta=false;
+        cerrarcarta();
     }else{
-        carta.setAttribute("src","images/carta-abierta.png");
-        cartadiv.removeClass("carta-wrapper");
-        cartadiv.addClass("carta-wrapper-open");
-        cartaAbierta=true;
+        abrircarta();
     }
     
 });
+
+function showinvitation(){
+    cartadiv.append('<img id="invitacion" src="images/invitacion-ejemplo.jpg">');
+}
+
+function hiddeinvitation(){
+    $("#invitacion").remove();
+}
+
+function abrircarta(){
+        carta.setAttribute("src","images/carta-abierta.png");
+        cartadiv.removeClass("carta-wrapper");
+        cartadiv.addClass("carta-wrapper-open");
+        showinvitation();
+        cartaAbierta=true;
+}
+
+function cerrarcarta(){
+        carta.setAttribute("src","images/carta-cerrada.png");
+        cartadiv.removeClass("carta-wrapper-open");
+        cartadiv.addClass("carta-wrapper");
+        hiddeinvitation();
+        cartaAbierta=false;
+}
