d6720e490291008bd8d526d20f9384499bc2ead3
Anthony Diaz <anthony.diaz@creativedrive.com>
2020-03-03 13:42:55 -0600

    ubicacion agregada correciones de boton sombra

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var cartaAbierta = false;
 var carta = document.getElementById("carta");
 var cartadiv = $("#carta-div");
-
+var mapa = $("#mapa")
 $(document.getElementById("carta")).click(function(){
     
     if(cartaAbierta){
@@ -39,7 +39,7 @@ function cerrarcarta(){
 /* IDEA FOR ICON FROM HERE -> http://dribbble.com/shots/1003867-Dribbble-Invitation?list=popular&offset=2 */
 
 /* Trigger Animation */
-$('a').on('click', function() {
+$('#abrir').on('click', function() {
     $('.container').addClass('is-active');
     $(this).remove();
 });
